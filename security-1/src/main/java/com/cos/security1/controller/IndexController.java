dcd688fc469e85bbb2af0f01d72aa9f007762f5d
sysim <liv660n@gmail.com>
2023-08-07 14:18:28 +0900

    add Security Config

--- security-1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security-1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,14 +2,41 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"", "/"})
     public String index() {
-        System.out.println("hello world");
-
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    } @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료!!";
     }
 }
