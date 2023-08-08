6a5c367ad36381c7f21527e6e3954a8784c69c41
sysim <liv660n@gmail.com>
2023-08-08 13:44:31 +0900

    권한처리

--- security-1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security-1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.cos.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true) //secured, preAuthorize, postAuthorize 활성화
 public class SecurityConfig {
 
     @Bean
--- security-1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security-1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.cos.security1.controller;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -53,5 +55,17 @@ public class IndexController {
         user.setPassword(encodePassword);
         userRepository.save(user);
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "데이터 정보";
     }
 }
